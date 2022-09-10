2f69886e457d0d329f3fee1a6a3442b8e927131b
Gerfey <sanjia@live.ru>
2022-09-10 18:15:27 +0700

    Отделил повторяющийся метод

--- src/pages/Registration/registrationPage.ts
+++ src/pages/Registration/registrationPage.ts
@@ -1,51 +1,14 @@
 import Components from '../../utils/Components';
-import {Validator} from '../../utils/Validator';
 import template from './registration.hbs';
 import './registration.scss';
+import sendDataForm from '../../utils/sendDataForm';
 
 export class RegistrationPage extends Components {
     constructor() {
         super({
             sendValues: () => {
                 const element = this.getContent();
-
-                const form = element?.querySelector('form');
-
-                if (form !== undefined) {
-                    const formInputs = form?.querySelectorAll('.form-item');
-
-                    const data: Record<string, unknown> = {};
-
-                    const validator = new Validator();
-
-                    let sendForm = true;
-
-                    Array.from(formInputs).forEach((formInput) => {
-                        const input = formInput?.querySelector('input');
-
-                        if (input) {
-                            data[input.name] = input.value;
-
-                            const resultValidation = validator.validate(input.name, input.value);
-
-                            const error = formInput?.querySelector('.form-item__div');
-
-                            if (resultValidation.result === false) {
-                                if (error !== undefined) {
-                                    error.textContent = resultValidation.message;
-                                }
-
-                                sendForm = false;
-                            } else {
-                                error.textContent = '';
-                            }
-                        }
-                    });
-
-                    if (sendForm) {
-                        console.log(data);
-                    }
-                }
+                sendDataForm(element);
             },
         });
     }
