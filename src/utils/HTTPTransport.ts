a04bd9c0e38d6bedc365e1c5a2caeaba7a7d0771
Gerfey <sanjia@live.ru>
2022-09-10 18:27:00 +0700

    Правка http клиента

--- src/utils/HTTPTransport.ts
+++ src/utils/HTTPTransport.ts
@@ -1,4 +1,4 @@
-export enum Method {
+enum METHOD {
     GET = 'GET',
     POST = 'POST',
     PUT = 'PUT',
@@ -6,80 +6,126 @@ export enum Method {
     DELETE = 'DELETE'
 }
 
-type Options = {
-    method: Method;
-    data?: any;
-};
+type RequestOptions = {
+    method?: METHOD;
+    headers?: Record<string, string>;
+    data?: Record<string, any>;
+    isFormData?: boolean;
+} | Record<string, never>;
 
-export default class HTTPTransport {
-    protected endpoint: string;
+type Request = (
+    url: string,
+    options?: RequestOptions,
+    timeout?: number
+) => Promise<XMLHttpRequest>;
 
-    constructor(endpoint: string) {
-        this.endpoint = endpoint;
-    }
+function queryStringify(data = {}) {
+    const items = Object.entries(data);
 
-    public get<Response>(path = '/'): Promise<Response> {
-        return this.request<Response>(this.endpoint + path);
-    }
+    let stringParams = '?';
+    items.forEach((value, index, array) => {
+        stringParams += `${value[0]}=${value[1]}`;
 
-    public post<Response = void>(path: string, data?: unknown): Promise<Response> {
-        return this.request<Response>(this.endpoint + path, {
-            method: Method.POST,
-            data,
-        });
-    }
+        if (array.length - 1 !== index) {
+            stringParams += '&';
+        }
+    });
 
-    public put<Response = void>(path: string, data: unknown): Promise<Response> {
-        return this.request<Response>(this.endpoint + path, {
-            method: Method.PUT,
-            data,
-        });
-    }
+    return stringParams;
+}
 
-    public patch<Response = void>(path: string, data: unknown): Promise<Response> {
-        return this.request<Response>(this.endpoint + path, {
-            method: Method.PATCH,
-            data,
-        });
-    }
+class HTTPTransport {
+    get: Request = (url, options = {}, timeout?) => this.request(
+        url,
+        {
+            ...options,
+            method: METHOD.GET,
+        },
+        timeout,
+    );
 
-    public delete<Response>(path: string): Promise<Response> {
-        return this.request<Response>(this.endpoint + path, {
-            method: Method.DELETE,
-        });
-    }
+    post: Request = (url, options = {}, timeout?) => this.request(
+        url,
+        {
+            ...options,
+            method: METHOD.POST,
+        },
+        timeout,
+    );
 
-    private request<Response>(url: string, options: Options = {method: Method.GET}): Promise<Response> {
-        const {method, data} = options;
+    put: Request = (url, options = {}, timeout?) => this.request(
+        url,
+        {
+            ...options,
+            method: METHOD.PUT,
+        },
+        timeout,
+    );
+
+    patch: Request = (url, options = {}, timeout?) => this.request(
+        url,
+        {
+            ...options,
+            method: METHOD.PATCH,
+        },
+        timeout,
+    );
+
+    delete: Request = (url, options = {}, timeout?) => this.request(
+        url,
+        {
+            ...options,
+            method: METHOD.DELETE,
+        },
+        timeout,
+    );
+
+    request: Request = (url, options = {}, timeout = 5000) => {
+        const {headers = {}, method, data, isFormData = false,} = options;
 
         return new Promise((resolve, reject) => {
+            if (!method) {
+                reject(new Error('Не указан метод запроса'));
+                return;
+            }
+
             const xhr = new XMLHttpRequest();
-            xhr.open(method, url);
+            const isGet = method === METHOD.GET;
 
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status < 400) {
-                        resolve(xhr.response);
-                    } else {
-                        reject(xhr.response);
-                    }
-                }
-            };
-
-            xhr.onabort = () => reject({reason: 'abort'});
-            xhr.onerror = () => reject({reason: 'network error'});
-            xhr.ontimeout = () => reject({reason: 'timeout'});
-
-            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.open(
+                method,
+                isGet && Boolean(data) ?
+                    `${url}${queryStringify(data)}` :
+                    url,
+            );
 
             xhr.withCredentials = true;
-            xhr.responseType = 'json';
 
-            if (method === Method.GET || !data) {
+            if (!isFormData) {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            }
+
+            Object.keys(headers).forEach((key) => {
+                xhr.setRequestHeader(key, headers[key]);
+            });
+
+            xhr.onload = () => {
+                resolve(xhr);
+            };
+
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+
+            xhr.timeout = timeout;
+            xhr.ontimeout = reject;
+
+            if (isGet || !data) {
                 xhr.send();
             } else {
-                xhr.send(JSON.stringify(data));
+                xhr.send((isFormData ? data : JSON.stringify(data)) as XMLHttpRequestBodyInit);
             }
         });
-    }
+    };
 }
+
+export default new HTTPTransport();
